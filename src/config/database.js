adaa4a215688d400bf5befdf6ae49b53e7d5b1b1
Carlos Limón <climon@tectos.com.mx>
2024-09-04 11:10:22 -0600

    Se comentan lineas

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
+// Actualizando URL de conección
 mongoose.connect('MONGO_URL');
 
 const db = mongoose.connection;
 
+// Configurando excepción gestionada
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Connected to MongoDB');
